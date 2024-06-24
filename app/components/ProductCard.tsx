83db3bd38bb7091747abe2f07ed166ab0ea607f2
rferreiramaceno <rferreiramaceno@gmail.com>
2024-06-24 14:43:58 -0600

    completed table using daisyUI

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,11 @@
 // "use client";
 import React from "react";
 import AddToCart from "./AddToCart";
+// import styles from './ProductCard.module.css';
 
 const ProductCard = () => {
   return (
+    // className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'
     <div>
       {/* <button onClick={() => console.log("Hi")}>Add to Cart</button> */}
       <AddToCart />
